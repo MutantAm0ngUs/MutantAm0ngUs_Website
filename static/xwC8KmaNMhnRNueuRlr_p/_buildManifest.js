c2dfeca903e03e7ba170985fa15e98b0a5d4ba91
MutantAm0ngUs <119432361+MutantAm0ngUs@users.noreply.github.com>
2022-11-29 17:57:03 +0100

    Add files via upload

---
+++ static/xwC8KmaNMhnRNueuRlr_p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9db3617a04bb29c8.css","static/chunks/pages/index-9e8fd45d3b0fb097.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
